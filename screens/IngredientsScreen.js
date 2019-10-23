2b266cbf4de07ea3686f077806b1449e478d8423
abentertainment <argertboja@yahoo.com>
2019-10-23 16:28:12 +0200

    Added explanation comments

--- screens/IngredientsScreen.js
+++ screens/IngredientsScreen.js
@@ -10,54 +10,67 @@
 import React, { useState } from 'react';
 import { Button, Image, Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
 import SelectMultiple from 'react-native-select-multiple';
+// Import data
 import Colors from '../constants/Colors';
 import { INGREDIENTS } from '../data/ingredients_data';
 import Icons from '../data/product_icons_data';
 
+
 const IngredientsScreen = props => {
 
+    // State for saving the selected ingredients
     const [selectedIngredients, setSelectedIngredients] = useState([]);
 
+    // Receiving ingredient data from local files
     const ingreds = INGREDIENTS;
 
+    // Changing selected ingredients array on click on checkbox
     onSelectionsChange = (ingredient) => {
         setSelectedIngredients(ingredient);
-      }
+    }
 
+    // Rendering list items (ingredients)
     const renderLabel = (label, style) => {
         return (
-          <View style={styles.ingredientContainer}>
-            <View style={{marginLeft: 10}}>
-              <Text style={styles.ingredientName}>{label}</Text>
+            <View style={styles.ingredientContainer}>
+                <View style={{ marginLeft: 10 }}>
+                    <Text style={styles.ingredientName}>{label}</Text>
+                </View>
+                <Image source={Icons[label]} />
             </View>
-            <Image source={Icons[label]} />
-          </View>
         )
-      }
-    
+    }
+
+    // Main view which displays igredients
     return (
-        <ScrollView> 
-          <SelectMultiple
-            items={ingreds}
-            renderLabel={renderLabel}
-            selectedItems={selectedIngredients}
-            onSelectionsChange={onSelectionsChange} 
-            checkboxSource = {require('../assets/images/icons/checkbox-unchecked.png')}
-            selectedCheckboxSource = {require('../assets/images/icons/checkbox-checked.png')}/>
-          <Button 
-            style={styles.button}
-            title='Kërko' 
-            onPress={() => {props.navigation.navigate({
-                routeName: 'Results', 
-                params: {
-                    selectedIDs : selectedIngredients
-                    }
-                })}
-            }/>
+        <ScrollView>
+            <SelectMultiple
+                items={ingreds}
+                renderLabel={renderLabel}
+                selectedItems={selectedIngredients}
+                onSelectionsChange={onSelectionsChange}
+                checkboxSource={require('../assets/images/icons/checkbox-unchecked.png')}
+                selectedCheckboxSource={require('../assets/images/icons/checkbox-checked.png')} />
+
+            {/* Button for going to next screen and sending selected ingredients*/}
+            <Button
+                style={styles.button}
+                title='Kërko'
+                onPress={() => {
+                    props.navigation.navigate({
+                        routeName: 'Results',
+                        params: {
+                            selectedIDs: selectedIngredients
+                        }
+                    })
+                }
+                }
+            />
         </ScrollView>
     );
 }
 
+// Navigation of screens
 IngredientsScreen.navigationOptions = {
     headerTitle: 'Përbërësit',
     headerStyle: {
@@ -66,6 +79,7 @@ IngredientsScreen.navigationOptions = {
     headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
 }
 
+// Styling
 const styles = StyleSheet.create({
     ingredientContainer: {
         flex: 1,
@@ -73,7 +87,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingLeft: 10,
         flexDirection: 'row'
-    }, 
+    },
     button: {
         backgroundColor: Colors.primary,
     },
